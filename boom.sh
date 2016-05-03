e93190c50187ada22c7a19fcbf971e28140b09c4
Donovan Tengblad <purplefish32@gmail.com>
2016-05-03 09:46:39 +0200

    Remove unused script

--- boom.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker stop claroline-mysql && docker rm claroline-mysql
-docker stop claroline-web && docker rm claroline-web
-
-docker run --name claroline-mysql -e MYSQL_ROOT_PASSWORD=toor -d mysql/mysql-server:5.7
-docker build -t claroline/distribution . && docker run -p 88:80 --name claroline-web --link claroline-mysql:mysql -d claroline/distribution && docker exec -it claroline-web bash
